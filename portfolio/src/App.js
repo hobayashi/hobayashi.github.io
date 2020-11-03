92d91be78f99c02096c93c2edfb11d8c4a5199b4
Hideaki Obayashi <h.o6ayashi@gmail.com>
2020-11-03 16:09:16 +0900

    add: portfolio items

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import KeyDrummerCaptured from './images/KeyDrummerCaptured.png';
+import DarkKakugeCheckerCaptured from './images/DarkKakugeCheckerCaptured.jpg';
+import FightingBoardCaptured from './images/FightingBoardCaptured.webp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hobayashi's Portfolios</h1>
+        <dl>
+          <dt><a target="brank" href="https://github.com/hobayashi/SoundSamplerVerE">Key Drummer</a></dt>
+          <dd>Simple Sound Player.</dd>
+          <dd>Using Electron, TypeScript.</dd>
+        </dl>
+        <img src={KeyDrummerCaptured} alt=""></img>
+        <dl>
+          <dt><a target="brank" href="https://chrome.google.com/webstore/detail/dark-kakuge-checker/keaigfdapmnmnndndekncagmndhlppnp?hl=ja">Dark Kakuge-Checker</a></dt>
+          <dd>Adapt Dark Mode UI to Kakuge-Checker.</dd>
+          <dd>Using Google Extentions, TypeScript.</dd>
+        </dl>
+        <img src={DarkKakugeCheckerCaptured} alt=""></img>
+        <dl>
+          <dt><a target="brank" href="https://play.google.com/store/apps/details?id=com.companyname.fightingboard">Fighting Board</a></dt>
+          <dd>Score Board App for Fighing Games.</dd>
+          <dd>Using C#, Xamarin.Android</dd>
+        </dl>
+        <img src={FightingBoardCaptured} alt=""></img>
       </header>
     </div>
   );
